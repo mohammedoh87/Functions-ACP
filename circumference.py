fc9e5d1a97ff787af882d20aec9c19c18991325b
mohammedoh87 <aamina.oh87@gmail.com>
2024-09-27 16:02:04 +0300

    Functions ACP Correction

--- circumference.py
+++ circumference.py
@@ -1,9 +1,7 @@
 def circumference():
+    diameter = int(input("Enter the length of the diameter: "))
+    radius = diameter / 2    
+    circum = 2 * 3.142 * radius
     print("The circumference of the circle is: ", circum)
-
-diameter = int(input("Enter the length of the diameter: "))
-radius = diameter / 2
-
-circum = 2 * 3.142 * radius
-
+    
 circumference()
